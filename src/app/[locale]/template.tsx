e1e1b592a95098c444effde4fb726d2a0c475bc2
suhaibbaba <suhaib.m.baba@gmail.com>
2025-11-05 00:28:14 +0200

    No skelton

--- src/app/[locale]/template.tsx
+++ src/app/[locale]/template.tsx
@@ -3,5 +3,6 @@ import { Suspense } from "react";
 import Loading from "@/components/ui/Loading";
 
 export default function Template({ children }: { children: React.ReactNode }) {
-  return <Suspense fallback={<Loading />}>{children}</Suspense>;
+  // return <Suspense fallback={<Loading />}>{children}</Suspense>;
+  return children;
 }
